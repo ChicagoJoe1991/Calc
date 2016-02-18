8a3b8710ba824c25627ad078e3ba4044698b9eee
ChicagoJoe1991 <dabears1.1.1@gmail.com>
2016-02-17 22:12:44 -0600

    add onClick methods to every button except clear and equals

--- app/src/main/java/com/example/walter/calc/CalculatorActivity.java
+++ app/src/main/java/com/example/walter/calc/CalculatorActivity.java
@@ -11,7 +11,8 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
-public String num1;
+public String text1="", text2="", op="";
+    public Boolean first=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +22,147 @@ public String num1;
 
     public void onClickSeven(View view){
         TextView textMessage = (TextView) findViewById(R.id.textView);
-        num1 += "7";
-        textMessage.setText(num1);
+        if(first) {
+            text1 += "7";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "7";
+            textMessage.setText(text2);
+        }
+
     }
+    public void onClickEight(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "8";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "8";
+            textMessage.setText(text2);
+        }
+
+    }
+    public void onClickNine(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "9";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "9";
+            textMessage.setText(text2);
+        }
+
+    }
+    public void onClickFour(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "4";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "4";
+            textMessage.setText(text2);
+        }
+
+    }
+    public void onClickFive(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "5";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "5";
+            textMessage.setText(text2);
+        }
+
+    }
+    public void onClickSix(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "6";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "6";
+            textMessage.setText(text2);
+        }
+
+    }
+    public void onClickOne(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "1";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "1";
+            textMessage.setText(text2);
+        }
+
+    }
+    public void onClickTwo(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "2";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "2";
+            textMessage.setText(text2);
+        }
+
+    }
+    public void onClickThree(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "3";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "3";
+            textMessage.setText(text2);
+        }
+
+    }
+    public void onClickZero(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        if(first) {
+            text1 += "0";
+            textMessage.setText(text1);
+        }
+        else {
+            text2 += "0";
+            textMessage.setText(text2);
+        }
+    }
+    public void onClickAdd(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        first = false;
+        op="add";
+        textMessage.setText("");
+    }
+    public void onClickSubtract(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        first = false;
+        op="subtract";
+        textMessage.setText("");
+    }
+    public void onClickMultiply(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        first = false;
+        op="multiply";
+        textMessage.setText("");
+    }
+    public void onClickDivide(View view){
+        TextView textMessage = (TextView) findViewById(R.id.textView);
+        first = false;
+        op="add";
+        textMessage.setText("");
+    }
+
 
 }
